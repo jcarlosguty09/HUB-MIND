01cdb9329e79252496f988678c80b71295909b11
jcarlosguty09 <jcarlosguty.09@gmail.com>
2026-07-27 20:35:05 -0600

    Update cdata.js

--- api/iclock/cdata.js
+++ api/iclock/cdata.js
@@ -20,6 +20,13 @@ export default async function handler(req, res) {
   const pad = n => String(n).padStart(2, '0');
  const t = `${now.getFullYear()}-${pad(now.getMonth()+1)}-${pad(now.getDate())} ${pad(now.getHours())}:${pad(now.getMinutes())}:${pad(now.getSeconds())}`;
 
+ const path = q.path || '';
+
+  // El device manda a /iclock/registry -> responder RegistryCode
+  if (path === 'registry' || req.url.includes('registry')) {
+    return res.status(200).send(`RegistryCode=OK\n`);
+  }
+
   if (req.method === 'GET') {
     return res.status(200).send(`GET OPTION FROM: ${sn}\nStamp=9999\nOpStamp=9999\nErrorDelay=30\nDelay=10\nTransTimes=00:00;14:05\nTransInterval=1\nTransFlag=1111111111\nRealtime=1\nServerVer=2.2.14\nTimeZone=-6\n`);
   }
